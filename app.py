f91d8177f92296449285a270405686097932fa0d
Sean Reidy <smreidy113@gmail.com>
2025-06-15 17:03:22 -0400

    Added category functionality

--- app.py
+++ app.py
@@ -7,23 +7,35 @@ import pandas as pd
 app = Flask(__name__)
 
 df = pd.read_json("database/plants.jsonl", lines=True)
+types_subcats = df['type'].unique().tolist()
+care_level_subcats = df['care_level'].unique().tolist()
+category_hierarchy = {'Type': 'type', 'Care Level': 'care_level'}
+subcategories = {'type': types_subcats, 'care_level': care_level_subcats}
+
 
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
 
-@app.route("/category/<string:category>")
-def api_categories(category):
-    return {"content": "You selected the " + category + " category."}
+@app.route("/<string:category_value>/")
+def category_page(category_value):
+    return render_template("category.html")
 
-@app.route("/category/<string:category>/subcat/<string:subcat>")
+@app.route("/api/<string:category_value>/")
+def api_category(category_value):
+    return jsonify(subcategories[category_value])
+
+@app.route("/<string:category_name>/<string:subcat>")
 def api_subcategories(category, subcat):
     return {"content": "You selected the " + category + " and the " + subcat + " sub-category."}
 
-@app.route("/getcategories")
+@app.route("/api/getcategories")
 def api_getcategories():
-    return jsonify(['Herb', 'Begonia', 'Shrub'])
+    return jsonify(category_hierarchy)
 
+@app.route("/getsubcategories/<string:category_value>")
+def api_getsubcategories(category_value):
+    return jsonify(category_hierarchy[category_value])
 
 @app.route("/")
 def home():
